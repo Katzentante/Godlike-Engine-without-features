7f572ef6b845cbcc589800ec5b58693a27f915e9
Oskar Ohlenmacher <katzentante@protonmail.com>
2023-03-24 19:18:17 +0100

    Changed camera

--- src/camera/camera.rs
+++ src/camera/camera.rs
@@ -1,13 +1,13 @@
 use crate::maths::vec3::Vec3;
 
 pub struct Camera {
-    pub fov: f32,
+    pub fovy: f32,
     pub aspect_ratio: f32,
     pub near: f32,
     pub far: f32,
 
-    pub eye: Vec3,
-    pub at: Vec3,
+    pub pos: Vec3,
+    pub target: Vec3,
     pub up: Vec3,
 }
 
