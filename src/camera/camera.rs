eaa97495745e4cadb0d3008b058871e2673d5d5e
Oskar Ohlenmacher <katzentante@protonmail.com>
2023-03-28 21:21:18 +0200

    Added cam.up calc funtion

--- src/camera/camera.rs
+++ src/camera/camera.rs
@@ -11,7 +11,23 @@ pub struct PerspectiveCamera {
     pub up: Vec3,
 }
 
-pub struct OrthographicCamera{
-
+impl PerspectiveCamera {
+    // FIXME works only if all x y and z are non zero
+    pub fn calc_up(&mut self) {
+        if self.target.z - self.pos.z != 0.0 {
+            self.up.x = self.target.x - self.pos.x;
+            self.up.y = self.target.y - self.pos.y;
+            self.up.z = -(self.up.x.powi(2) + self.up.y.powi(2)) / (self.target.z - self.pos.z);
+        } else if self.target.y - self.pos.y != 0.0 { // ea.z is 0
+            self.up.x = self.target.x - self.pos.x;
+            self.up.z = self.target.z - self.pos.z;
+            self.up.y = -(self.up.x.powi(2) + self.up.z.powi(2)) / (self.target.y - self.pos.y);
+        } else { // ea.z and ea.y is 0
+            self.up.z = self.target.z - self.pos.z;
+            self.up.y = self.target.y - self.pos.y;
+            self.up.x = -(self.up.z.powi(2) + self.up.y.powi(2)) / (self.target.x - self.pos.x);
+        }
+    }
 }
 
+pub struct OrthographicCamera {}
